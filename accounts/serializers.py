af61025e3b812e444e810c1d82db34275f8b5019
kenzi26 <mokwenyekene1@yahoo.com>
2023-10-31 09:03:05 +0100

    updated serializers

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -5,17 +5,16 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 
 class UserCreateSerializer(BaseUserCreateSerializer):
     class Meta(BaseUserCreateSerializer.Meta):
-        fields = ["id", "username", "password", "email", "full_name", "phone"]
+        fields = ["id","first_name", 'last_name', "email","password","role"]
         
 class AdminUserCreateSerializer(BaseUserCreateSerializer):
     class Meta(BaseUserCreateSerializer.Meta):
-        fields = ["id", "username", "password", "email", "full_name", "role", "phone"]
-
+        ["id","first_name", 'last_name', "email","password","role"]
 
 class UserSerializer(BaseUserSerializer): 
     class Meta(BaseUserSerializer.Meta):
 
-        fields = ["id", "email", "username", "date_joined",  "role", "is_active"]
+        fields = ["id","first_name", "last_name", "email", "date_joined", "role", "is_active"]
 
 
 
@@ -27,6 +26,7 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
         # Add custom claims
         token['role'] = user.role  # assuming a "role" field on the user model
         token['email'] = user.email
-        token['username'] = user.username
+        token['first_name'] = user.first_name
         token['is_staff'] = user.is_staff
         return token
+
