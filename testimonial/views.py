98c7df591df356dd3bce902064b10dff5b467642
kenzi26 <mokwenyekene1@yahoo.com>
2023-11-01 01:50:01 +0100

    Refact: removed paginations in testimonials viewset

--- testimonial/views.py
+++ testimonial/views.py
@@ -2,10 +2,7 @@ from rest_framework import generics, viewsets, mixins, pagination
 from .serializers import TestimonialSerializer, Testimonial
 
 # Create your views here.
-class CustomPagination(pagination.PageNumberPagination):
-    page_size = 200
-    page_size_query_param = 'page_size'
-    max_page_size = 200
+
 
 class TestimonialViewSet(viewsets.ViewSet,
                        generics.GenericAPIView,
@@ -14,4 +11,3 @@ class TestimonialViewSet(viewsets.ViewSet,
     
     queryset = Testimonial.objects.all()
     serializer_class = TestimonialSerializer
-    pagination_class = CustomPagination
