1e0630d564fd2cda5d3cc9771fd16c388b39428b
kenzi26 <mokwenyekene1@yahoo.com>
2023-11-01 01:37:40 +0100

    Refact: removed pagination in blog viewset

--- blog/views.py
+++ blog/views.py
@@ -8,17 +8,13 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.parsers import FormParser, MultiPartParser
 
 
-class CustomPageNumberPagination(PageNumberPagination):
-    page_size = 15
-    page_size_query_param = 'page_size'
-    max_page_size = 20
+
 
 class BlogViewSet(viewsets.ModelViewSet):
 
     queryset = Blog.objects.filter(is_deleted=False)
     serializer_class = BlogSerializer
-    pagination_class = CustomPageNumberPagination 
-    parser_classes = [FormParser, MultiPartParser]
+    
 
     
 
