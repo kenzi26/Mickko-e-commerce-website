3b2bb47e544f77c6eb1986b2594da395d29872b6
Kenechukwu Mokwenye <mokwenyekene1@yahoo.com>
2023-11-06 23:50:31 +0100

    null to testimonial image

--- testimonial/models.py
+++ testimonial/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Testimonial(models.Model):
     full_name= models.CharField(max_length=255)
-    profile_picture= models.ImageField(upload_to='testimonial/')
+    profile_picture= models.ImageField(upload_to='testimonial/', null=True, blank=True)
     company= models.CharField(max_length=255, null=True, blank=True)
     content= models.CharField(max_length=500)
 
