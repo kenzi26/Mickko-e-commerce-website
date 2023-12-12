3687ec9d99fdbcf37d5422dc0cc56b626eb136ac
Kenechukwu Mokwenye <mokwenyekene1@yahoo.com>
2023-12-12 18:38:26 +0100

    Refact: contact us

--- biodata/models.py
+++ biodata/models.py
@@ -5,7 +5,7 @@ from django.contrib.postgres.fields import ArrayField
 class BioData(SingletonModel):
     """ Hero model that enables the home page and it's content to be dynamic. It only takes one instance"""
     work_emails= ArrayField(models.EmailField(), help_text="Enter email addresses separated by commas") 
-    phone_numbers = (models.CharField(max_length=15))
+    phone_numbers = (models.CharField(max_length=10))
     sales_email = models.EmailField()
     contact_us_email = models.EmailField()
     footer_text = models.TextField()
